cdc8b8918e8b0fa656f2fbee5a7b4d27e62f1d45
philimon-reset <philimonderib2019@gmail.com>
2021-12-06 12:36:19 +0300

    mend

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -11,6 +11,6 @@ import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> typing.Any:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """ Returns: _asyncio.Task: _asyncio.Task object"""
     return asyncio.create_task(wait_random(max_delay))
