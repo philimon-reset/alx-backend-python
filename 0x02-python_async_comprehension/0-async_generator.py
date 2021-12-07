6b4118fc4e87ff8fece35e1375b8b0d258098d1a
philimon-reset <philimonderib2019@gmail.com>
2021-12-07 07:13:01 +0300

    final push #

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, float, str]:
+async def async_generator() -> Generator[float, None, None]:
     """ Generate a random number after await
 
     Yields:
