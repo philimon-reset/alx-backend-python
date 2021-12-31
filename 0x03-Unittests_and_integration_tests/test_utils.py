ef3d648962f45110524d628c4cb3e2d9f899d88c
philimon-reset <philimonderib2019@gmail.com>
2021-12-31 07:45:33 +0300

    type anotation #

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,14 +3,13 @@
     """
 
 import unittest
-from typing import Generator
+from typing import Mapping, Sequence
 from utils import access_nested_map
 from parameterized import parameterized, parameterized_class
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """ Test Access nested map function
-
     Args:
         unittest ([type]): Inherited unittest method
     """
@@ -19,6 +18,6 @@ class TestAccessNestedMap(unittest.TestCase):
         [{"a": {"b": 2}}, ("a",), 1],
         [{"a": {"b": 2}}, ("a", "b"), 2]
         ])
-    def test_access_nested_map(self, nested, path, expected):
+    def test_access_nested_map(self, nested: Mapping, path: Sequence, expected: int) -> None:
         """ test function """
         self.assertEqual(access_nested_map(nested, path), expected)
