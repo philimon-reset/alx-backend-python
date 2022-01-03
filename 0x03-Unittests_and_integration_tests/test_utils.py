95cf15fc04ddc829008a75ea712f6a8f6dac5e94
philimon-reset <philimonderib2019@gmail.com>
2022-01-03 13:59:16 +0300

    patch and mock #

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,9 @@
     """
 
 import unittest
+from unittest.mock import MagicMock, patch
 from typing import Mapping, Sequence
-from utils import access_nested_map
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized, parameterized_class
 
 
@@ -23,7 +24,7 @@ class TestAccessNestedMap(unittest.TestCase):
             nested: Mapping,
             path: Sequence,
             expected: int) -> None:
-        """ test function """
+        """ test function access nested map with valid values """
         self.assertEqual(access_nested_map(nested, path), expected)
 
     @parameterized.expand([
@@ -31,8 +32,25 @@ class TestAccessNestedMap(unittest.TestCase):
         [{"a": 1}, ("a", "b")]
     ])
     def test_access_nested_map_exception(self, nested, path):
-        """ Testing errors raised
+        """ Testing errors raised on access nested map
         """
         with self.assertRaises(KeyError) as expected_E:
             access_nested_map(nested, path)
         self.assertEqual(expected_E.exception.args[0], path[-1])
+
+
+class TestGetJson(unittest.TestCase):
+    """ Getjson testing class
+
+    Args:
+        unittest ([type]): Inherited unittest method
+    """
+    @parameterized.expand([
+        ["http://example.com", {"payload": True}],
+        ["http://holberton.io", {"payload": False}]
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ Testing get json valid values """
+        with patch("test_utils.get_json", return_value=test_payload) as m:
+            main_result = get_json(test_url)
+        self.assertEqual(main_result["payload"], test_payload["payload"])
